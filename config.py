3fcfa7a11bdcea603dc068d74e684a8d94a6b5e5
vivek reddy <vivekreddy97@gmail.com>
2019-12-12 21:23:51 -0500

    Task: created unit test

---
+++ config.py
@@ -0,0 +1,14 @@
+
+ class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+ app_config = {
+
+    'testing': Testing
+ }
